6a0be6689aaeedefc80a683a9b3ded601b2a7271
Whitestone2019 <whitestonedevhub@whitestones.co.in>
2025-07-18 16:35:20 +0530

    "bug"

--- src/main/java/com/wssl/los/model/LoanType.java
+++ src/main/java/com/wssl/los/model/LoanType.java
@@ -16,40 +16,41 @@ public class LoanType {
     @Column(name = "description", nullable = false)
     private String description;
 
-    @Lob
     @Column(name = "approval_setup", columnDefinition = "TEXT")
     private String approvalSetup; // stores JSON string
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLoanType() {
+		return loanType;
+	}
+
+	public void setLoanType(String loanType) {
+		this.loanType = loanType;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getApprovalSetup() {
+		return approvalSetup;
+	}
+
+	public void setApprovalSetup(String approvalSetup) {
+		this.approvalSetup = approvalSetup;
+	}
+
     // Getters & Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLoanType() {
-        return loanType;
-    }
-
-    public void setLoanType(String loanType) {
-        this.loanType = loanType;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getApprovalSetup() {
-        return approvalSetup;
-    }
-
-    public void setApprovalSetup(String approvalSetup) {
-        this.approvalSetup = approvalSetup;
-    }
+    
 }
